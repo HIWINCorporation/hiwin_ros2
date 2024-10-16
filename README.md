98d57e3fa9b36a9004e0c1a8064484fcd0fe47b3
hiwin_robot <hiwin_robot@hiwin.tw>
2024-10-16 15:22:30 +0800

    Add RA series robot MoveIt package

--- README.md
+++ README.md
@@ -9,5 +9,52 @@ ROS2 stack for HIWIN robots.
 Branch naming follows the ROS distribution they are compatible with. -devel branches may be unstable. Releases are made from the distribution branches (humble, iron).
 
 # System Requirements
+### TBD.
 
-# Installation
+# Install From Source
+### 1.Install requirements:
+```bash
+sudo apt install -y \
+ros-humble-ament-cmake \
+ros-humble-ament-cmake-clang-format \
+ros-humble-angles \
+ros-humble-ros2-controllers \
+ros-humble-ros2-control \
+ros-humble-ros2-control-test-assets \
+ros-humble-controller-manager \
+ros-humble-control-msgs \
+ros-humble-control-toolbox \
+ros-humble-generate-parameter-library \
+ros-humble-joint-state-publisher \
+ros-humble-joint-state-publisher-gui \
+ros-humble-moveit \
+ros-humble-pinocchio \
+ros-humble-realtime-tools \
+ros-humble-xacro \
+ros-humble-hardware-interface \
+ros-humble-ros-gz \
+```
+
+### 2.Create a ROS 2 workspace:
+```bash
+mkdir -p ~/colcon_ws/src
+```
+### 3.Clone repo and build packages:
+```bash
+source /opt/ros/humble/setup.bash
+
+cd ~/colcon_ws
+
+git clone https://github.com/HIWINCorporation/hiwin_ros2.git src/hiwin_ros2
+
+colcon build --symlink-install
+
+source install/setup.sh
+```
+
+# Usage
+### Mock hardware
+The package can simulate hardware with the ``ros2_control``MockSystem. For more details see [ros2_control](https://control.ros.org/rolling/doc/ros2_control/hardware_interface/doc/mock_components_userdoc.html) documentation for more details.
+```bash
+ros2 launch hiwin_ra6_moveit_config ra6_moveit.launch.py ra_type:=ra610_1869
+```
---
+++ hiwin_ra6_moveit_config/launch/ra6_moveit.launch.py
@@ -0,0 +1,155 @@
+from pathlib import Path
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition
+from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+from moveit_configs_utils import MoveItConfigsBuilder
+
+
+def launch_setup():
+    controller_spawner_timeout = LaunchConfiguration("controller_spawner_timeout")
+    launch_rviz = LaunchConfiguration("launch_rviz")
+    rviz_config = LaunchConfiguration("rviz_config")
+    ra_type = LaunchConfiguration("ra_type")
+    tf_prefix = LaunchConfiguration("tf_prefix")
+
+    moveit_config = (
+        MoveItConfigsBuilder("hiwin_ra6")
+        .robot_description(
+            file_path="config/ra6.urdf.xacro",
+            mappings={"name": "hiwin", "ra_type": ra_type, "tf_prefix": tf_prefix},
+        )
+        .robot_description_semantic(Path("config") / "ra6.srdf")
+        .robot_description_kinematics(Path("config") / "kinematics.yaml")
+        .trajectory_execution(Path("config") / "moveit_controllers.yaml")
+        .to_moveit_configs()
+    )
+
+    robot_controllers = PathJoinSubstitution(
+        [FindPackageShare("hiwin_ra6_moveit_config"), "config", "ros2_controllers.yaml"]
+    )
+
+    control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[
+            robot_controllers,
+        ],
+        output="both",
+        remappings=[
+            ("~/robot_description", "/robot_description"),
+        ],
+    )
+
+    # Spawn controllers
+    def controller_spawner(controllers):
+        return Node(
+            package="controller_manager",
+            executable="spawner",
+            arguments=[
+                "--controller-manager",
+                "/controller_manager",
+                "--controller-manager-timeout",
+                controller_spawner_timeout,
+            ]
+            + controllers,
+        )
+
+    controllers_active = [
+        "joint_state_broadcaster",
+        "manipulator_controller",
+    ]
+    controller_spawners = [controller_spawner(controllers_active)]
+
+    # Start the actual move_group node/action server
+    move_group_node = Node(
+        package="moveit_ros_move_group",
+        executable="move_group",
+        output="screen",
+        parameters=[
+            moveit_config.to_dict(),
+            {"use_sim_time": False},
+        ],
+    )
+
+    # Publish TF
+    robot_state_publisher = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        output="both",
+        parameters=[
+            moveit_config.robot_description,
+            {"use_sim_time": False},
+        ],
+    )
+
+    # RViz
+    rviz_config = PathJoinSubstitution(
+        [FindPackageShare("hiwin_ra6_moveit_config"), "config", "moveit.rviz"]
+    )
+    rviz_node = Node(
+        package="rviz2",
+        condition=IfCondition(launch_rviz),
+        executable="rviz2",
+        name="rviz2_moveit",
+        output="log",
+        arguments=["-d", rviz_config],
+        parameters=[
+            moveit_config.robot_description,
+            moveit_config.robot_description_semantic,
+            moveit_config.robot_description_kinematics,
+            moveit_config.planning_pipelines,
+            moveit_config.joint_limits,
+        ],
+    )
+
+    nodes_to_start = [
+        robot_state_publisher,
+        control_node,
+        move_group_node,
+        rviz_node,
+    ] + controller_spawners
+
+    return nodes_to_start
+
+
+def generate_launch_description():
+    declared_arguments = []
+
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "controller_spawner_timeout",
+            default_value="10",
+            description="Timeout used when spawning controllers.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument("launch_rviz", default_value="true", description="Launch RViz?")
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "ra_type",
+            description="Typo/series of used RA robot.",
+            choices=[
+                "ra605_710",
+                "ra610_1355",
+                "ra610_1869",
+            ],
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "tf_prefix",
+            default_value="",
+            description="Prefix of the joint names, useful for \
+            multi-robot setup. If changed than also joint names in the controllers' configuration \
+            have to be updated.",
+        )
+    )
+
+    return LaunchDescription(declared_arguments + launch_setup())
